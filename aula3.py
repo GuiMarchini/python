d9169e91014d59f7a10b35a1970fbaba905d4f34
Guilherme Marchini <gmportolima25@gmail.com>
2026-05-15 11:23:13 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/curriculo')
+def curriculo():
+    return render_template('home.html')
+
+@app.route('/cotemig/<nome>')
+def cotemig(nome):
+    return f'Olá, {nome}! Bem-vindo ao Cotemig.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
